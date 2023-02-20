5e7c72d37ef1a5c443fdc4156dbe9dfeb7e17534
Bongani Gumede <Bongani.Gumede@hotmail.com>
2023-02-20 16:45:34 +0200

    Adding create employee rest api

--- src/main/java/com/mtesazi/springboot/controller/EmployeeController.java
+++ src/main/java/com/mtesazi/springboot/controller/EmployeeController.java
@@ -4,14 +4,13 @@ package com.mtesazi.springboot.controller;
 import com.mtesazi.springboot.model.Employee;
 import com.mtesazi.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -23,9 +22,15 @@ public class EmployeeController {
         return employeeRepository.findAll();
     }
 
+    // Create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
     //Delete employee rest api
 
-    // Create employee rest api
+
 
     //Update employee rest api
 }
