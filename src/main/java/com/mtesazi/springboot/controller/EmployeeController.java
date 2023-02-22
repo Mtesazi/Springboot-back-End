18015a475843bbd2a4ac4a6a7440d6101ffdd497
Bongani Gumede <Bongani.Gumede@hotmail.com>
2023-02-22 10:33:08 +0200

    Adding Get employees by Id rest api

--- src/main/java/com/mtesazi/springboot/controller/EmployeeController.java
+++ src/main/java/com/mtesazi/springboot/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.mtesazi.springboot.controller;
 
 
+import com.mtesazi.springboot.exception.ResourceNotFoundException;
 import com.mtesazi.springboot.model.Employee;
 import com.mtesazi.springboot.repository.EmployeeRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +29,15 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    //get employee by Id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+        return ResponseEntity.ok(employee);
     }
 
     //Delete employee rest api
